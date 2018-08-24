a5d5e120a329401309515ee1064ca3f2eb765a98
Homeye <kornjoni@gmail.com>
2018-08-24 16:52:45 +0200

    created a simple REST controller

--- springboot-in-10-steps/src/main/java/com/in28minutes/springboot/basics/springbootin10steps/SpringbootIn10StepsApplication.java
+++ springboot-in-10-steps/src/main/java/com/in28minutes/springboot/basics/springbootin10steps/SpringbootIn10StepsApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootIn10StepsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootIn10StepsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootIn10StepsApplication.class, args);
+    }
 }

e3928496cad74b97952806f360d9c031dace0562
Homeye <kornjoni@gmail.com>
2018-08-24 15:02:52 +0200

    Dependency Injection with Spring Annotations

--- spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsApplication.java
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsApplication.java
@@ -2,18 +2,27 @@ package com.in28minutes.spring.basics.springin5steps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringIn5StepsApplication {
 
+    //What are the beans? @Component
+    //What are the dependencies of a bean? @Autowired
+    //Where to search for beans? No need --> @SpringBootApplication 's package and package's packages
+
     public static void main(String[] args) {
 
-//        BinarySearchImpl binarySearch = new BinarySearchImpl();
-//        BinarySearchImpl binarySearch = new BinarySearchImpl(new BubbleSortAlgorithm());
-        BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
+//        BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
+//        int result = binarySearch.binarySearch(new int[]{12, 4, 6}, 3);
+//        System.out.println(result);
+
+//        SpringApplication.run(SpringIn5StepsApplication.class, args);
+
+        //Application Context
+        ApplicationContext applicationContext = SpringApplication.run(SpringIn5StepsApplication.class, args);
+        BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
         int result = binarySearch.binarySearch(new int[]{12, 4, 6}, 3);
         System.out.println(result);
-
-        SpringApplication.run(SpringIn5StepsApplication.class, args);
     }
 }

74c42da9b5a21fa6b7edbcaeccafda8b78b4e464
Homeye <kornjoni@gmail.com>
2018-08-27 13:46:50 +0200

    added POST rest service

--- student-services/src/main/java/com/in28minutes/springboot/controller/StudentController.java
+++ student-services/src/main/java/com/in28minutes/springboot/controller/StudentController.java
@@ -3,10 +3,15 @@ package com.in28minutes.springboot.controller;
 import com.in28minutes.springboot.model.Course;
 import com.in28minutes.springboot.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -24,5 +29,20 @@ public class StudentController {
     public Course retrieveDetailsForCourse(@PathVariable String studentId,
                                            @PathVariable String courseId) {
         return studentService.retrieveCourse(studentId, courseId);
+    }
+
+    @PostMapping("/students/{studentId}/courses")
+    public ResponseEntity<Void> registerStudentForCourse(
+            @PathVariable String studentId, @RequestBody Course newCourse) {
+
+        Course course = studentService.addCourse(studentId, newCourse);
+
+        if (course == null)
+            return ResponseEntity.noContent().build();
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path(
+                "/{id}").buildAndExpand(course.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }

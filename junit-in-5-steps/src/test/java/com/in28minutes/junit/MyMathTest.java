b248c5b1b5444aac3d0c4a34edef9eb328564a95
Homeye <kornjoni@gmail.com>
2018-08-27 16:28:42 +0200

    trying out some common annotations

--- junit-in-5-steps/src/test/java/com/in28minutes/junit/MyMathTest.java
+++ junit-in-5-steps/src/test/java/com/in28minutes/junit/MyMathTest.java
@@ -1,5 +1,9 @@
 package com.in28minutes.junit;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,14 +12,36 @@ public class MyMathTest {
 
     private MyMath myMath = new MyMath();
 
+    @Before
+    public void before() {
+        System.out.println("@Before running before every tests");
+    }
+
+    @After
+    public void after() {
+        System.out.println("@After running after every tests");
+    }
+
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("@BeforeClass running before the first test");
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("@AfterClass running after the last test");
+    }
+
     @Test
     public void sumWith3Numbers() {
+        System.out.println("Test1");
         int result = myMath.sum(new int[]{1, 2, 3});
         assertEquals(6, result);
     }
 
     @Test
     public void sumWith1Number() {
+        System.out.println("Test2");
         assertEquals(3, myMath.sum(new int[]{3}));
     }
 }

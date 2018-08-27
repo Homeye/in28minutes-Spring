1335ac9e2bd354d51bfd1e2633fdfd99c41ad028
Homeye <kornjoni@gmail.com>
2018-08-27 16:02:56 +0200

    created tests

--- junit-in-5-steps/src/test/java/com/in28minutes/junit/MyMathTest.java
+++ junit-in-5-steps/src/test/java/com/in28minutes/junit/MyMathTest.java
@@ -6,8 +6,16 @@ import static org.junit.Assert.*;
 
 public class MyMathTest {
 
+    private MyMath myMath = new MyMath();
+
     @Test
-    public void test() {
-        //Absence of failure is success
+    public void sumWith3Numbers() {
+        int result = myMath.sum(new int[]{1, 2, 3});
+        assertEquals(6, result);
+    }
+
+    @Test
+    public void sumWith1Number() {
+        assertEquals(3, myMath.sum(new int[]{3}));
     }
 }
